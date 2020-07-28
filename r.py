f17ea1666615fe36e8bfe241172abac39036daa6
BILLY CHANG <ts04574870@gmail.com>
2020-07-28 19:06:04 +0900

    update version

--- r.py
+++ r.py
@@ -1,6 +1,8 @@
 import random
 r = random.randint(1,100)
+count = 0
 while True:
+    count += 1 # count = count + 1
     answer = input('請輸入數字: ')
     answer = int(answer)
     if answer > r :
@@ -9,5 +11,6 @@ while True:
         print('不對,再大一點!')
     elif answer == r:
         print('恭喜你終於猜對了!!')
+        print('這是你猜的', count, '次')
         break
-    
+    print('這是你猜的', count, '次')
